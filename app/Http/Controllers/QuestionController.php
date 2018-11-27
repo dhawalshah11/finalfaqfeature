377cb1944c5b0242615f170dcfb0d19cdcba36bf
dhawalshah11 <ds792@njit.edu>
2018-11-27 12:55:41 -0500

    Created and edited QuestionForm, QuestionController , Answer.php, Question.php, answerForm.blade, home.blade and questionForm.blade.

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -27,22 +27,35 @@ class QuestionController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
     public function create()
     {
-        //
+        $question = new Question;
+        $edit = FALSE;
+        return view('questionForm', ['question' => $question,'edit' => $edit  ]);
     }
-
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+
     public function store(Request $request)
     {
-        //
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+        ]);
+        $input = request()->all();
+        $question = new Question($input);
+        $question->user()->associate(Auth::user());
+        $question->save();
+        return redirect()->route('home')->with('message', 'IT WORKS!');
+        // return redirect()->route('questions.show', ['id' => $question->id]);
     }
-
     /**
      * Display the specified resource.
      *
@@ -54,18 +67,18 @@ class QuestionController extends Controller
     {
         return view('question')->with('question', $question);
     }
-
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
-        //
-    }
 
+    public function edit(Question $question)
+    {
+        $edit = TRUE;
+        return view('questionForm', ['question' => $question, 'edit' => $edit ]);
+    }
     /**
      * Update the specified resource in storage.
      *
@@ -73,9 +86,18 @@ class QuestionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+
+    public function update(Request $request, Question $question)
     {
-        //
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+        ]);
+        $question->body = $request->body;
+        $question->save();
+        return redirect()->route('questions.show',['question_id' => $question->id])->with('message', 'Saved');
     }
 
     /**
