1b54205b76cde0aa43741ff8564a43a03023072f
dhawalshah11 <ds792@njit.edu>
2018-12-16 19:09:41 -0500

    Initail Setup, Answer Notification file created

---
+++ app/Notifications/answernoti.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Http\Request;
+
+class answernoti extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $request = Request::capture();
+        $askPath = $request->path();
+        $Stringway = explode("/", $askPath);
+        return (new MailMessage)
+            ->line('You have new answer')
+            ->action('Take a watch', \route('questions.show', $Stringway[1]))
+            ->line('Keep answering!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
